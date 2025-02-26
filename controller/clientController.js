9fa1c301b5c440ec11720e15112bf321c9a8ac31
Vishnu palanivel M <mvishnupalanivel@gmail.com>
2025-02-26 11:04:06 +0530

    Update clientController.js
    1.upload

--- controller/clientController.js
+++ controller/clientController.js
@@ -2,15 +2,16 @@ const asyncHandler = require('express-async-handler');
 const Client = require('../models/clientSchema');
 
 const createClient = asyncHandler(async(req, res)=>{
-    const { name, amount, email,phone_number,select_type } = req.body;
+    const {name, amount, email,phone_number,select_type,date,schemes} = req.body;
  
     const newClient = await Client.create({
         name: name,
         amount: amount,
         email: email,
         phone_number:phone_number,
-        select_type:select_type
-  
+        select_type:select_type,
+        date:date,
+        schemes:schemes,
     });
     if(newClient){
         res.status(200).json({_id: newClient.id, name: newClient.name})
@@ -28,7 +29,47 @@ const getClients = asyncHandler(async(req, res)=>{
         console.log("Error Fetching Clients", error);
         res.status(500).json({message: 'Server Error'})
     }
-})
+});
 
 
-module.exports = { createClient, getClients }
+const getCltDtl = asyncHandler(async (req, res) => {
+    try {
+        let { page, limit, sortBy, sortOrder } = req.query;
+
+        // Default values if parameters are not provided
+        page = parseInt(page) || 1; // Default: Page 1
+        limit = parseInt(limit) || 20; // Default: 20 records per page
+        sortBy = sortBy || 'createdAt'; // Default sorting field is 'createdAt'
+        sortOrder = sortOrder === 'desc' ? -1 : 1; // Default: ascending order
+
+        const skip = (page - 1) * limit; // Calculate how many records to skip
+
+        // Fetch clients with sorting, skipping, and limiting records
+        const clients = await Client.find()
+            .sort({ [sortBy]: sortOrder }) // Dynamic sorting
+            .skip(skip) // Skip records for pagination
+            .limit(limit); // Limit the number of results
+
+        res.json(clients);
+    } catch (error) {
+        console.log("Error Fetching Clients", error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+
+const deleteAll = asyncHandler(async (req, res) => {
+    try {
+        const result = await Client.deleteMany({}); // Deletes all clients
+
+        res.json({ message: `${result.deletedCount} clients deleted successfully` });
+    } catch (error) {
+        console.log("Error Deleting Clients", error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
+
+
+
+module.exports = { createClient, getClients,getCltDtl,deleteAll}
