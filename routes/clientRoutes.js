3eff7d8d592a1d43d907b04cedac1f70e54f2a3f
Vishnu palanivel M <mvishnupalanivel@gmail.com>
2025-02-26 11:05:56 +0530

    Update clientRoutes.js
    added clients models

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createClient, getClients  } = require('../controller/clientController');
+const { createClient, getClients,getCltDtl,deleteAll  } = require('../controller/clientController');
 const router = express.Router();
 
-router.post( '/client', createClient);
-router.get( '/clients', getClients);
+router.post('/client',createClient);
+router.get('/clients',getClients);
+router.get('/clientsort',getCltDtl);
+router.delete('/clientdelete',deleteAll);
 
 module.exports = router;
